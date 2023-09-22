29ba0b5dadaee38a04cea42da59dce14bb05803a
raccoonD0G <138660933+raccoonD0G@users.noreply.github.com>
2023-09-22 09:43:56 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -7,24 +7,15 @@
 
 int main()
 {
-	int start;
-	int end;
-
+	int start, end;
 	std::cin >> start >> end;
 	std::vector<bool> boolArr(end + 1, true);
-	boolArr[1] = false;
+	boolArr[0] = boolArr[1] = 0;
 
 	for (int i = 2; i * i <= end; i++)
 	{
-		if (boolArr[i])
-		{
-			for (int j = i * i; j <= end; j += i)
-				boolArr[j] = false;
-		}
+		if(boolArr[i]) for (int j = i * i; j <= end; j += i) boolArr[j] = false;
 	}
 
-	for (int i = start; i < end + 1; i++)
-	{
-		if (boolArr[i]) std::cout << i << '\n';
-	}
+	for (int i = start; i <= end; i++) if (boolArr[i]) std::cout << i << '\n';
 }
