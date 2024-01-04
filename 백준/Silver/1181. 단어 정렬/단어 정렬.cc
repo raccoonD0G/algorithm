81e9238a1099f0c7290e1b876a8a96503c0b5e4e
raccoonD0G <138660933+raccoonD0G@users.noreply.github.com>
2024-01-05 08:06:17 +0900

    Update 단어 정렬.cc

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -5,31 +5,25 @@
 
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
+int main()
+{
+	int num;
+	cin >> num;
 
-    vector<string> words(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> words[i];
-    }
+	vector<string> arr;
+	string tmp;
+	for (int i = 0; i < num; i++)
+	{
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
 
-    // 사전 순으로 먼저 정렬
-    sort(words.begin(), words.end());
+	sort(arr.begin(), arr.end());
+	arr.erase(unique(arr.begin(), arr.end()), arr.end());
+	sort(arr.begin(), arr.end(), [](string& a, string& b) -> bool {if (a.length() == b.length()) return a < b;  return a.length() < b.length(); });
 
-    // 중복 제거
-    words.erase(unique(words.begin(), words.end()), words.end());
-
-    // 길이에 따라 정렬 (길이가 같으면 사전 순으로 이미 정렬됨)
-    sort(words.begin(), words.end(), [](const string &a, const string &b) {
-        if (a.length() == b.length()) return a < b;
-        return a.length() < b.length();
-    });
-
-    // 결과 출력
-    for (const auto &word : words) {
-        cout << word << '\n';
-    }
-
-    return 0;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << '\n';
+	}
 }
