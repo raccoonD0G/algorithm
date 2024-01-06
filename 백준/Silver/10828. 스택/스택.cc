86d05986656dfd46dc849f526ca324a1993d4ee3
raccoonD0G <138660933+raccoonD0G@users.noreply.github.com>
2024-01-06 20:51:12 +0900

    [Silver IV] Title: 스택, Time: 4 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	stack<int> m_stack;
+
+	int m_num;
+	cin >> m_num;
+	
+	vector<string> m_inputArr;
+	string m_tmp;
+
+	cin.ignore();
+
+	for (int i = 0; i < m_num; i++)
+	{
+		getline(cin, m_tmp);
+		m_inputArr.push_back(m_tmp);
+	}
+
+	int m_new;
+	for (int i = 0; i < m_num; i++)
+	{
+		if (m_inputArr[i].find("push") != string::npos)
+		{
+			m_new = stoi(m_inputArr[i].substr(4, m_inputArr[i].size()));
+			m_stack.push(m_new);
+		}
+		else if (m_inputArr[i] == "top")
+		{
+			if (m_stack.size() > 0) cout << m_stack.top();
+			else cout << -1;
+			cout << "\n";
+		}
+		else if (m_inputArr[i] == "pop")
+		{
+			if (m_stack.size() > 0)
+			{
+				cout << m_stack.top();
+				m_stack.pop();
+			}
+			else cout << -1;
+			cout << "\n";
+		}
+		else if (m_inputArr[i] == "size")
+		{
+			cout << m_stack.size();
+			cout << "\n";
+		}
+		else if (m_inputArr[i] == "empty")
+		{
+			if (m_stack.size() == 0) cout << 1;
+			else cout << 0;
+			cout << "\n";
+		}
+	}
+}
