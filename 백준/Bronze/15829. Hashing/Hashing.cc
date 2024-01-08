3973fc9c156f4a09509cf60d249fe2103e62260c
raccoonD0G <138660933+raccoonD0G@users.noreply.github.com>
2024-01-09 02:51:35 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,6 +2,9 @@
 #include <string>
 #include <cmath>
 
+#define M 1234567891
+#define R 31
+
 using namespace std;
 
 int main()
@@ -12,11 +15,14 @@ int main()
 	string str;
 	cin >> str;
 
-	int answer = 0;
+	unsigned long long answer = 0;
+	unsigned long long powBuf = 1;
 	for (int i = 0; i < length; i++)
 	{
-		answer += (str[i] - 'a' + 1) * pow(31, i);
+		answer = (answer + ((str[i] - 'a' + 1) * powBuf) % M) % M;
+		powBuf = (powBuf * R) % M;
 	}
 
-	cout << answer;
+	cout << answer % M;
+	return 0;
 }
